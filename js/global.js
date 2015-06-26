0be094100867e3230cf9844c6c64d617ef32e1bb
cromozooom <cromozooom@gmail.com>
2015-06-26 17:35:12 +0200

    adde files

---
+++ js/global.js
@@ -0,0 +1,29 @@
+/*
+Theme by: WebThemez.com
+Note: Please use our back link in your site
+*/
+$( function() {
+        var endDate = "july 6, 2015 13:00:00";
+
+        $('.countdown.simple').countdown({ date: endDate });
+
+        $('.countdown.styled').countdown({
+          date: endDate,
+          render: function(data) {
+            $(this.el).html(
+              "<div class='col-xs-6 col-md-3'><div class='clock'>" + this.leadingZeros(data.days, 3) + " <span>giorni</span></div></div><div class='col-xs-6 col-md-3'><div class='clock'>" + this.leadingZeros(data.hours, 2) + " <span>ore</span></div></div><div class='col-xs-6 col-md-3'><div class='clock'>" + this.leadingZeros(data.min, 2) + " <span>minuti</span></div></div><div class='col-xs-6 col-md-3'><div class='clock'>" + this.leadingZeros(data.sec, 2) + " <span>secondi</span></div></div>");
+          }
+        });
+
+        $('.countdown.callback').countdown({
+          date: +(new Date) + 10000,
+          render: function(data) {
+            $(this.el).text(this.leadingZeros(data.sec, 2) + " sec");
+          },
+          onEnd: function() {
+            $(this.el).addClass('ended');
+          }
+        }).on("click", function() {
+          $(this).removeClass('ended').data('countdown').update(+(new Date) + 10000).start();
+        });
+      });

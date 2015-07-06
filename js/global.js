67dfcd5ba1b2219693d1a2404b4c5040b09cf3b3
cromozooom <cromozooom@gmail.com>
2015-07-06 09:51:21 +0200

    update orario

--- js/global.js
+++ js/global.js
@@ -3,7 +3,7 @@ Theme by: WebThemez.com
 Note: Please use our back link in your site
 */
 $( function() {
-        var endDate = "july 6, 2015 13:00:00";
+        var endDate = "july 6, 2015 12:00:00";
 
         $('.countdown.simple').countdown({ date: endDate });
 
